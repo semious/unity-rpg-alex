e2a16de789b162499f84259fd9bca493686c1239
semious <semious@google.com>
2025-02-20 21:56:52 +0800

    update
    更新项目配置和Unity版本
    
    - 更新 `Assembly-CSharp.csproj` 中的项目 GUID 和 Unity 版本。
    - 在 `Player.cs` 中添加 `xInput` 变量以获取水平输入。
    - 修改 `SampleScene.unity` 中多个对象的位置和物理属性。
    - 更新 `manifest.json` 中 `com.unity.ide.rider` 的版本。
    - 在 `packages-lock.json` 中更新多个包的版本。
    - 更新 `ProjectVersion.txt` 中的 Unity 编辑器版本。
    - 在 `unity-rpg-alex.sln` 中添加新的项目配置。

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float xInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //Debug.Log(Input.GetAxisRaw("Horizontal"));  
+        xInput = Input.GetAxisRaw("Horizontal");
     }
 }
