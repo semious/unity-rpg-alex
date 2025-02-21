5542efe6367c2f829bafba0856825dbb3facf64e
semious <semious@163.com>
2025-02-21 16:42:17 +0800

    update

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+   
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private bool isMoving;
+
+
     private float xInput;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start");
+        //Debug.Log("Start");
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -17,5 +29,16 @@ public class Player : MonoBehaviour
 
         //Debug.Log(Input.GetAxisRaw("Horizontal"));  
         xInput = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+       isMoving = rb.velocity.x != 0;
+
+        anim.SetBool("isMoving", isMoving);
     }
 }
